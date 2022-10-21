e5ad078bd6534343cd2b3754abf775316571729e
PrasannaKumar-19 <1912019@nec.edu.in>
2022-10-21 10:02:27 +0530

    init

--- WEB-INF/classes/NewPassword.java
+++ WEB-INF/classes/NewPassword.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-
+import java.sql.*;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
